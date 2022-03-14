c01c6719a3e100a01d09eb9f39b734a792f9ceb8
Ivy Vo <52213218+dongnghi05@users.noreply.github.com>
2022-03-14 01:04:24 -0700

    Update-Create following in IUserService and implement them in UserService class (missing unversioned Files)

---
+++ MovieShopMVC/Controllers/CastController.cs
@@ -0,0 +1,21 @@
+using ApplicationCore.Contracts.Respositories;
+using ApplicationCore.Contracts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieShopMVC.Controllers;
+
+public class CastController : Controller
+{
+    private readonly ICastService _castService;
+
+    public CastController(ICastService castService)
+    {
+        _castService = castService;
+    }
+
+    public async Task<IActionResult> Details(int id)
+    {
+        var castDetails = await _castService.GetCastDetails(id);
+        return View(castDetails);
+    }
+}
---
+++ MovieShopMVC/Infra/MovieShopExceptionMiddleware.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace MovieShopMVC.Infra
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class MovieShopExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<MovieShopExceptionMiddleware> _logger;
+
+        public MovieShopExceptionMiddleware(RequestDelegate next, ILogger<MovieShopExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            // logic 
+            _logger.LogInformation("Inside exception Middleware");
+            // if there is no exception then call next middleware
+          
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Exception happened, hanled here");
+
+                // if there is exception then do the logging
+               await HandleExceptionLogic(httpContext, ex);
+            }
+
+        }
+
+        private async Task HandleExceptionLogic(HttpContext httpContext, Exception exception)
+        {
+            _logger.LogError("Something wenr wrong");
+
+            // get the exception details
+            var exceptionDetails = new 
+            { 
+                ExceptionMessage = exception.Message,
+                ExceptionStackTrace = exception.StackTrace,
+                ExceptionType = exception.GetType(),
+                ExceptionDetails = exception.InnerException?.Message,
+                ExceptionDateTime = DateTime.UtcNow,
+                Path = httpContext.Request.Path,
+                HttpMethod = httpContext.Request.Method,
+                User = httpContext.User.Identity.IsAuthenticated ? httpContext.User.Identity.Name : null,
+
+            };
+
+            // log the above object details to text or json file using Serilog
+
+            _logger.LogError(exceptionDetails.ExceptionMessage);
+            httpContext.Response.Redirect("/home/error");
+            await Task.CompletedTask;
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MovieShopExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMovieShopExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MovieShopExceptionMiddleware>();
+        }
+    }
+}
